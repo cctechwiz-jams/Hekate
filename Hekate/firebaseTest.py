7efee1fad9bce3aae01750bbdb2a15390ccd89f4
Josh Maxwell <cctechwiz@gmail.com>
2014-11-21 12:27:41 -0700

    Python project in pycharm. Currently just tests some basic functions of accessing data in firebase.

---
+++ Hekate/firebaseTest.py
@@ -0,0 +1,9 @@
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://hekate.firebaseio.com/', None)
+
+# results = firebase.get("Commands", None)
+
+# firebase.put("Commands", "cmd", "168.168.1.1")
+
+# firebase.delete("Commands", 'cmd')
+# firebase.delete("Commands", 'Ping')
