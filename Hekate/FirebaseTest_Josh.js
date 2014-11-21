a07a6ad78e7777b0aae55e979fad06b951f2a196
Josh Maxwell <cctechwiz@gmail.com>
2014-11-21 16:25:20 -0700

    Further parsing of the returned dto.

--- Hekate/FirebaseTest_Josh.js
+++ Hekate/FirebaseTest_Josh.js
@@ -1,4 +1,5 @@
-console.log("hello world");
+console.log("Making changes in Firebase...");
+
 var usrId = "User1535";
 var command = "do something more again!";
 
@@ -6,3 +7,4 @@ var Firebase = require('firebase');
 var Hekate = new Firebase('https://hekate.firebaseio.com');
 var User1 = Hekate.child(usrId);
 User1.set({command: command});
+
--- Hekate/ListeningToFirebaseTest_Josh.js
+++ Hekate/ListeningToFirebaseTest_Josh.js
@@ -3,30 +3,35 @@ console.log("Listening for Firebase to update...")
 var Firebase = require('firebase');
 var Hekate = new Firebase('https://hekate.firebaseio.com');
 
+var user = '';
+
 Hekate.on("value", function (DataSnapshot) {
     console.log("key: ", DataSnapshot.key());
     console.log("val: ", DataSnapshot.val());
 
     console.log("hasChildren: ", DataSnapshot.hasChildren());
 
-    DataSnapshot.forEach(function(childSnapshot){
+    DataSnapshot.forEach(function (childSnapshot) {
+        user = childSnapshot.key();
         console.log("childkey:", childSnapshot.key());
         console.log("Childval:", childSnapshot.val());
 
         console.log("childhasChildren: ", childSnapshot.hasChildren());
-        childSnapshot.forEach(function(childchildSnapshot) {
+        childSnapshot.forEach(function (childchildSnapshot) {
             console.log("childchildkey:", childchildSnapshot.key());
             console.log("childchildval:", childchildSnapshot.val());
         });
 
     });
 
+    var data = DataSnapshot.exportVal();
+    printChange(data);
 
-//   printChange(snapshot.val());
-}, function (errorObject){
+
+}, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
 
-var printChange = function(data){
-  console.log(data);
+var printChange = function (data) {
+    console.log("Command to execute: ", data[user]["command"]);
 };
