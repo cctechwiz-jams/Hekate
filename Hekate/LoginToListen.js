d1db3b072d240bfb3af2dc71f85468339255f1d0
Josh Maxwell <cctechwiz@gmail.com>
2014-11-21 23:06:04 -0700

    Authentication words from the server side.

--- Hekate/LoginToListen.js
+++ Hekate/LoginToListen.js
@@ -1,30 +1,64 @@
 var Firebase = require('firebase');
 var Hekate = new Firebase('https://hekate.firebaseio.com');
 
-var email = '';
-var password = '';
+var UID = '';
 
-var readline1 = require('readline');
+var readline = require('readline');
 
-var rl1 = readline1.createInterface({
+var rl1 = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-rl1.question("Enter your email: ", function(answer) {
-    email = answer;
+rl1.question("Enter your UID:(Found in your Android Hekate settings) ", function(answer) {
+    UID = answer;
     rl1.close();
+    authenticateForFirebase();
 });
 
-var readline2 = require('readline');
+var authenticateForFirebase = function() {
+    console.log("email: ", UID);
 
-var rl2 = readline2.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-rl2.question("Enter your password:(Will be displayed in plain text!) ", function(answer) {
-    email = answer;
-    rl2.close();
-});
+    var FirebaseTokenGenerator = require("firebase-token-generator");
+    var tokenGenerator = new FirebaseTokenGenerator("pSFof16OWEXpaCR37Fx11d6C7RQZTyYFRtOaiNPH");
+    var token = tokenGenerator.createToken({uid: UID});
 
-console.log("email: ", email);
-console.log("password: ", password);
+    Hekate.authWithCustomToken(token, function(error, authData) {
+        if (error) {
+            console.log("Login Failed!", error);
+            throw new Error(error);
+        } else {
+            console.log("Login Succeeded!", authData);
+            startListening();
+        }
+    });
+};
+
+var startListening = function(){
+    var user = '';
+    var dataKey = '';
+    var dataProperty = '';
+
+    Hekate.child("User/"+UID).on("value", function (DataSnapshot) {
+        console.log("key: ", DataSnapshot.key());
+        console.log("val: ", DataSnapshot.val());
+
+        console.log("hasChildren: ", DataSnapshot.hasChildren());
+
+        if(DataSnapshot.hasChildren()) {
+            DataSnapshot.forEach(function (childSnapshot) {
+                user = childSnapshot.key();
+
+                if(childSnapshot.hasChildren()) {
+                    childSnapshot.forEach(function (childchildSnapshot) {
+                        dataKey = childchildSnapshot.key();
+                        dataProperty = childchildSnapshot.val();
+                    });
+                }
+
+            });
+        }
+
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+};
