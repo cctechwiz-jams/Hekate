686c49cfdb2b03d177642c1327cde7e12ebf61c5
Brance Boren <branceboren@gmail.com>
2015-07-27 05:37:10 -0600

    node project dependencies will be handled by package.json and npm install

---
+++ Hekate/example.js
@@ -0,0 +1,19 @@
+var gpio = require('onoff').Gpio,
+    pin_num = 8,
+    pin = new gpio(pin_num, 'out');
+
+pin.readSync(function(err, value){
+    if(err){
+        console.log("Something went wrong...");
+        throw err;
+    }
+
+    console.log(value);
+});
+
+function exit() {
+    pin.unexport();
+    process.exit();
+}
+
+process.on('SIGINT', exit);
